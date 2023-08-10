94f76c63a5bf2492976e09e279987188d91b5140
CarloVitto01 <vitto.recarlo@hotmail.com>
2023-08-10 19:44:58 +0200

    Nuovo sito parte 2

--- src/PVsito.tsx
+++ src/PVsito.tsx
@@ -10,6 +10,7 @@ import Logo_Whatsapp from './Logo_Whatsapp.png';
 import Logo_Telegram from './Logo_Telegram.png';
 import Logo_mail from './Logo_mail.png';
 import Logo_Instagram from './Logo_Instagram.png';
+import logo_carrello from './logo_carrello.jpg';
 
 function PVsito() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -30,8 +31,6 @@ function PVsito() {
   return (
     <div className="sfondo">
       <div>
-
-
         <div className="header">{/*HEADER*/}
           <div className="header-section">
             <button className="menu-button" onClick={toggleMenu}>
@@ -43,7 +42,7 @@ function PVsito() {
                   ✕
                 </button>
                 <ul className="menu-list">
-                  <li><a href="#">Link 1</a></li>
+                  <li><a href="#">Home</a></li>
                   <li><a href="#">Link 2</a></li>
                   <li>
                     <a href="#" onClick={() => toggleSubMenu(0)}>Link 3</a>
@@ -61,16 +60,15 @@ function PVsito() {
           <div className="header-section">
             <img src={logo} alt="Logo" className="Logo" />
           </div>
-          <div className="header-section"></div>
+          <div className="header-section header-section-right">
+    <img src={logo_carrello} alt="Logo_Carrello" className="Logo_Carrello" />
+  </div>
         </div>
-
-        <div className="riquadroStudente">
+        <div className="riquadroStudente">{/*RIQUADRO STUDENTE SULLO SCONTO DEL 10%*/}
           <div className="riquadroInternoStudente">
             <p className="textStudente">Se sei uno studente universitario hai il 10% di sconto!</p>
           </div>
         </div>
-
-
         <div className="containerRettangoli">
           <div className="rectangle">{/*GRAFICA*/}
             <div>
@@ -151,7 +149,34 @@ function PVsito() {
             </div>
           </div>
         </div>
-        <div className="footer">
+        <div className="chiSiamoRiquadro">
+          <div className="chiSiamoTitolo">
+            Chi siamo?
+          </div>
+          <div className="chiSiamoDescrizione">
+            Benvenuto in Photo and Vision, un'azienda formata da due ragazzi che hanno unito le loro passioni per dare vita a un'esperienza unica
+            nel campo della grafica, stampe, foto e video.<br />
+            Fondata da Carlo e Andrea, la nostra azienda è nata dalla voglia di trasformare le idee in realtà visive straordinarie.<br />
+            La nostra missione è semplice ma ambiziosa: soddisfare appieno le esigenze dei nostri clienti attraverso soluzioni visive personalizzate e di alta qualità.<br />
+            <br />
+            Sappiamo quanto sia importante per te comunicare il tuo messaggio in modo efficace e memorabile, ed è proprio qui che entriamo in gioco.<br />
+            Con anni di esperienza combinata nel settore della grafica e della produzione multimediale, siamo pronti ad affrontare qualsiasi sfida tu possa presentarci.<br />
+            Creiamo progetti che non solo rispondono alle tue esigenze, ma che anche catturano l'essenza della tua identità o del tuo marchio.<br />
+            Che tu stia cercando un logo distintivo, materiale promozionale accattivante o un video coinvolgente, il nostro team lavora in sinergia con te
+            per comprendere appieno le tue idee e trasformarle in opere d'arte visive che supereranno le tue aspettative.<br />
+            <br />
+            La qualità è al centro di tutto ciò che facciamo. Utilizziamo le ultime tecnologie e software di design per garantire che ogni dettaglio sia impeccabile.<br />
+            Dalla progettazione all'esecuzione, lavoriamo con dedizione per offrirti risultati che parlano da soli.<br />
+            <br />
+            E se c'è una cosa che ci spinge costantemente avanti, è il sorriso soddisfatto dei nostri clienti quando vedono il loro progetto prendere vita.<br />
+            Non si tratta solo di servizi, ma di relazioni. Vogliamo costruire connessioni durature con i nostri clienti, diventando i tuoi partner creativi di fiducia.<br />
+            La tua visione è la nostra priorità e lavoriamo instancabilmente per garantire che sia realizzata con cura e precisione.<br />
+            <br />
+            Unisciti a noi in questo viaggio creativo. Photo and Vision è qui per trasformare le tue idee in capolavori visivi, superando le aspettative e
+            creando risultati che durano nel tempo. Siamo pronti a mettere il nostro talento al tuo servizio e a realizzare insieme qualcosa di straordinario.<br />
+          </div>
+        </div>
+        <div className="footer">{/*FOOTER*/}
           <div className="footer-section">
             <img src={Logo_mail} alt="Logo1" className="footer-logo" />
             <p className="footer-text">PV.PHOTOANDVISION@GMAIL.COM</p>
@@ -171,7 +196,6 @@ function PVsito() {
             <p className="footer-text">ANDREA: +39 328 800 6210</p>
           </div>
         </div>
-
       </div>
     </div>
   );
