d56f035a51ea97ef500738bbf85d037e263dc948
Emanuele Coricciati <ema@Air-di-Emanuele.homenet.telecomitalia.it>
2023-08-15 12:53:33 +0200

    fixed header div

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./PVsito.css";
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
