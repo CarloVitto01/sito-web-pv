554a051f5e45e8a35684442cc360edc9bf946316
Emanuele Coricciati <ema@Air-di-Emanuele.homenet.telecomitalia.it>
2023-08-15 13:02:12 +0200

    added link to containers

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,34 @@
-import classes from './Card.module.css';
-const parse = require('html-react-parser');
+import classes from "./Card.module.css";
+import { Link } from "react-router-dom";
+const parse = require("html-react-parser");
 
-const Card: React.FC<{title: string, subtitle: string, img: string, text: string}> = (props) => {
+const Card: React.FC<{
+  title: string;
+  subtitle: string;
+  img: string;
+  text: string;
+}> = (props) => {
   return (
     <div className={classes["rectangle"]}>
-      {/*GRAFICA*/}
-      <div>
-        <p className={classes["titoloRiquadri"]}>{props.title}</p>
-      </div>
-      <div className={classes["immagineRiquadri"]}>
-        <img src={props.img} alt="logo_grafica" className={classes["logoRiquadri"]} />
-      </div>
-      <div className={classes["sottoTitoloRiquadro"]}>
-        <p className={classes["sottoTitolo"]}>{props.subtitle}</p>
-      </div>
-      <div className={classes["testoRiquadri"]}>
-        <p className={classes["testo"]}>
-            {parse(props.text)}
-        </p>
-      </div>
+      <Link to={props.title.toLowerCase() } className={classes['noUnderline']}>
+        {/*GRAFICA*/}
+        <div>
+          <p className={classes["titoloRiquadri"]}>{props.title}</p>
+        </div>
+        <div className={classes["immagineRiquadri"]}>
+          <img
+            src={props.img}
+            alt="logo_grafica"
+            className={classes["logoRiquadri"]}
+          />
+        </div>
+        <div className={classes["sottoTitoloRiquadro"]}>
+          <p className={classes["sottoTitolo"]}>{props.subtitle}</p>
+        </div>
+        <div className={classes["testoRiquadri"]}>
+          <p className={classes["testo"]}>{parse(props.text)}</p>
+        </div>
+      </Link>
     </div>
   );
 };
