31729add601bffd6c24ec5c7c6f930317fa61ce9
CarloVitto01 <128791502+CarloVitto01@users.noreply.github.com>
2024-03-05 00:23:02 +0100

    new price paper and color (#14)

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,9 @@ import { RangePagesData } from "./types/RangePagesData";
 //Constants
 
 
-const foglio = 0.028;
+const foglio = 0.021;
 const biancoNero: number = 0.013;
-const colore: number = 0.041;
+const colore: number = 0.045;
 const anelli = 1.5;
 const fascetta = 1;
 const ciappatura = 0.1;
