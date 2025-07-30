4233478381433c74b6e49ae95d480a68e39d2c34
CarloVitto01 <vitto.recarlo@hotmail.com>
2025-07-30 15:39:42 +0200

    feat: Aggiornato il layout del componente modal e migliorata la responsività; modificato il link nella sezione CardsSection

--- src/components/HomeComponents/CardsSection.tsx
+++ src/components/HomeComponents/CardsSection.tsx
@@ -6,7 +6,8 @@ const CardsSection: React.FC = () => (
     <div className={styles.grid}>
       <Link to="/printA4" className={styles.card}>A4</Link>
       <Link to="/printA3" className={styles.card}>A3</Link>
-      <Link to="/comingSoon" className={styles.card}>Coming Soon</Link>
+      <Link to="/contatti-servizi-foto-video" className={styles.card}>Contatti Servizi Foto/Video</Link>
+      <Link to="/comingSoon" className={styles.card}>Coming Soon...</Link>
     </div>
   </section>
 );
