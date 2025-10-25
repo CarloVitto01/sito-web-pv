94cead07b3bad230e9b37892dafcf5f1753e01ab
CarloVitto01 <vitto.recarlo@hotmail.com>
2025-10-25 12:37:05 +0200

    fix: import pdfjs and set worker source for PDF rendering

--- src/components/MultiInputComponents/MultiInput.tsx
+++ src/components/MultiInputComponents/MultiInput.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
 import classes from "./MultiInput.module.css";
 import { FileHandler } from "../../types/FileHandler";
-import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import Loading from "../LoadingComponents/Loading";
 import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
 import { AiOutlineClose } from "react-icons/ai";
 
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 
 interface PropsContainer {
